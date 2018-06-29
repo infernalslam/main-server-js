68be2767f20eb7aa94ae3bc1914dd1e9c02aee7c
infernalslam <infernal-slam@hotmail.com>
2018-06-29 16:43:22 +0700

    fix code

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,11 +1,13 @@
+const services = require('service-server')
 const accountController = {
   async getAccount (ctx) {
     ctx.body = 'ok post get account'
   }, 
   async postAccount (ctx) {
     const { form } = ctx.request.body
-    console.log(form)
-    const account = await ctx.db.models.account.create(form)
+    const account = await services.account.createAccount(form)
+    // console.log(form)
+    // const account = await ctx.db.models.account.create(form)
     ctx.body = account
   }
 }
